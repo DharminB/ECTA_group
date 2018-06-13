362a2e46dff2eb8a881ceb6e678c7264681bc0f4
Dharmin <dharmingeo@gmail.com>
2018-06-13 22:17:13 +0200

    edited files so that it works

--- assignment_3/A3-ShapeMatching-CodeHandout/my_cma_es.m
+++ assignment_3/A3-ShapeMatching-CodeHandout/my_cma_es.m
@@ -2,12 +2,13 @@ function output = my_cma_es(nacafoil)
 
 % Algorithm Parameters
 nGenes  = 32;
-lambda = 14;
+lambda = 10;
 sigma = 0.01;
-change_in_sigma = 0.01;
+change_in_sigma = 0.005;
 maxGen = 500;
 
 mu = floor(lambda/2);
+% weights = zeros([1,mu])+1/mu;
 weights = zeros([1,mu]);
 for i = 1:mu
     weights(i) = mu - i + 1;
@@ -17,7 +18,7 @@ mueff = 1/(sum(weights.^2));
 C = eye(nGenes);
 c_1 = 2/(nGenes^2);
 c_mu = min(mueff/(nGenes^2), 1-c_1);
-C_mu = 0;
+C_mu = zeros(nGenes, nGenes);
 meanInd = rand(1,32)-0.5;
 
 bestFit = zeros([maxGen, 1]);
@@ -28,81 +29,53 @@ successful_mutation = 0;
 prev_status = 0;
 p = 5;      % number of generation at which freq_success_mut is update
 
-% Create an individual (consisting of 32 y values between -0.5 and 0.5)
-% pop = (rand(popSize,32)-0.5);
 
 for iGen=1:maxGen
-    iGen
-    
     % Mutation
     mutated_children = cma_es_mutation(meanInd, sigma, lambda,C);
-%     size(mutated_children)
     fitness_mutated_children = mse(mutated_children, nacafoil);
     
     [fitness_sorted, indices] = sort(fitness_mutated_children);
     sorted_children = mutated_children(indices(1:mu),:);
-%     size(sorted_children)
-    temp = mse(meanInd, nacafoil) > fitness_sorted;
+
+    temp = mse(meanInd, nacafoil) > fitness_mutated_children;
     successful_mutation = successful_mutation + sum(temp);
     
     ind_minus_mean = sorted_children - meanInd;
-    meanInd = zeros(1,nGenes);
     for i = 1:mu
-        C_mu = C_mu + weights(i) * (ind_minus_mean(i) * ind_minus_mean(i)');
-        meanInd = meanInd + (weights(i)*sorted_children(i));
+        C_mu = C_mu + weights(i) * (ind_minus_mean(i,:)' * ind_minus_mean(i,:));
     end
     C_mu = C_mu/mu;
     
-    rank_mu_update = c_mu * (1/(sigma^2)) * C_mu;
-    y = ind_minus_mean(1)/sigma;
-    rank_1_update = c_1 * (y*y');
-    
+    rank_mu_update = c_mu * (1/(mueff^2)) * C_mu;
+    y = ind_minus_mean(1,:)/mueff;
+    rank_1_update = c_1 * (y'*y);
     C = (1 - c_1 - c_mu) * C + rank_1_update + rank_mu_update;
-%     C = (1 - c_1) * C + rank_1_update;
-%     C = (1 - c_mu) * C + rank_mu_update;
 
+    meanInd = weights*sorted_children;
     meanInd(meanInd>0.5) = 0.5; 
     meanInd(meanInd<-0.5) = -0.5; 
-    
     
     bestFit(iGen) = min(fitness_sorted);
     medianFit(iGen) = median(fitness_sorted);
     
-    
     if mod(iGen, p) == 0
-        successful_mutation;
-        freq_success_mut = successful_mutation / p
-        if freq_success_mut - 0.2 <= 0
-            curr_status = 1;
-        else
-            curr_status = 0;
-        end
-        if (prev_status == curr_status)
-            alpha = 1.1;
-        else
-            alpha = 0.9;
-        end
-        change_in_sigma = change_in_sigma + (alpha-1)/100;
-%         change_in_sigma = change_in_sigma*alpha;
+        freq_success_mut = successful_mutation / p;
         if freq_success_mut < 0.2
             sigma = sigma + change_in_sigma;
         elseif freq_success_mut > 0.2
             sigma = sigma - change_in_sigma;
         end
         successful_mutation = 0;
-        prev_status = curr_status;
-        if sigma > 5
-            sigma = 5;
-        elseif sigma < -5
-            sigma = -5;
-        end
-        sigma
     end
+    disp([iGen min(fitness_sorted) sigma])
+    
+%     plotFoil(nacafoil, meanInd);
 end
 % sorted_children(1)
 % append to output
 output.bestFit = bestFit;
 output.medianFit = medianFit;
-output.elite = sorted_children(1,:);
+output.elite = meanInd;
     
 end
--- assignment_3/A3-ShapeMatching-CodeHandout/my_cma_es_ep.m
+++ assignment_3/A3-ShapeMatching-CodeHandout/my_cma_es_ep.m
@@ -1,122 +1,81 @@
 function output = my_cma_es_ep(nacafoil)
 
-max_sigma = 0;
-min_sigma = 100;
+% Algorithm Parameters
+nGenes  = 32;
+lambda = 10;
+sigma = 0.03;
+maxGen = 500;
 
-N = 32;
-% number of objective variables/problem dimension
-xmean = rand(N,1);
-% objective variables initial point
-sigma = 0.5;
-% coordinate wise standard deviation (step-size)
-stopfitness = 10; % stop if fitness > stopfitness (minimization)
-% Strategy parameter setting: Selection
-lambda = 4+floor(3*log(N)); % population size, offspring number
-maxGen = 50;
-stopeval = maxGen*lambda;
+mu = floor(lambda/2);
+% weights = zeros([1,mu])+1/mu;
+weights = zeros([1,mu]);
+for i = 1:mu
+    weights(i) = mu - i + 1;
+end
+weights = weights/sum(weights);
+mueff = 1/(sum(weights.^2));
+C = eye(nGenes);
+c_1 = 2/(nGenes^2);
+c_mu = min(mueff/(nGenes^2), 1-c_1);
+C_mu = zeros(nGenes, nGenes);
+p_C = zeros(1, nGenes);
+c_C = 1/nGenes;
+p_sigma = zeros(1, nGenes);
+c_sigma = 1/nGenes;
+
+meanInd = rand(1,32)-0.5;
+
 bestFit = zeros([maxGen, 1]);
 medianFit = zeros([maxGen, 1]);
-% stop after stopeval number of function evaluations
 
-mu = lambda/2;
-% lambda=12; mu=3; weights = ones(mu,1); would be (3_I,12)-ES
-weights = log(mu+1/2)-log(1:mu)';
-% muXone recombination weights
-mu = floor(mu);
-% number of parents/points for recombination
-weights = weights/sum(weights);
-% normalize recombination weights array
-mueff=sum(weights)^2/sum(weights.^2); % variance-effective size of mu
-% Strategy parameter setting: Adaptation
-cc = (4+mueff/N) / (N+4 + 2*mueff/N); % time constant for cumulation for C
-cs = (mueff+2)/(N+mueff+5); % t-const for cumulation for sigma control
-c1 = 2 / ((N+1.3)^2+mueff); % learning rate for rank-one update of C
-cmu = 2 * (mueff-2+1/mueff) / ((N+2)^2+2*mueff/2); % and for rank-mu update
-damps = 1 + 2*max(0, sqrt((mueff-1)/(N+1))-1) + cs; % damping for sigma
-% Initialize dynamic (internal) strategy parameters and constants
-pc = zeros(N,1); ps = zeros(N,1);
-% evolution paths for C and sigma
-B = eye(N);
-% B defines the coordinate system
-D = eye(N);
-% diagonal matrix D defines the scaling
-C = B*D*(B*D)';
-% covariance matrix
-eigeneval = 0;
-% B and D updated at counteval == 0
-chiN=N^0.5*(1-1/(4*N)+1/(21*N^2)); % expectation of
-%||N(0,I)|| == norm(randn(N,1))
-% -------------------- Generation Loop --------------------------------
-counteval = 0; % the next 40 lines contain the 20 lines of interesting code
-iGen = 1;
-while counteval < stopeval
-    % Generate and evaluate lambda offspring
-    for k=1:lambda
-        arz(:,k) = randn(N,1); % standard normally distributed vector
-        arx(:,k) = xmean + sigma * (B*D * arz(:,k));
-        % add mutation
-        % Eq. 40
-        arfitness(k) = mse(arx(:,k)', nacafoil); % objective function call
-        counteval = counteval+1;
-    end
-    % Sort by fitness and compute weighted mean into xmean
-    [arfitness, arindex] = sort(arfitness); % minimization
-    xmean = arx(:,arindex(1:mu))*weights;
-    % recombination
-    % Eq. 42
-    zmean = arz(:,arindex(1:mu))*weights;
-    % == Dˆ-1*B’*(xmean-xold)/sigma
-    % Cumulation: Update evolution paths
-    ps = (1-cs)*ps + (sqrt(cs*(2-cs)*mueff)) * (B * zmean);
-    % Eq. 43
-    hsig = norm(ps)/sqrt(1-(1-cs)^(2*counteval/lambda))/chiN < 1.4+2/(N+1);
-    pc = (1-cc)*pc + hsig * sqrt(cc*(2-cc)*mueff) * (B*D*zmean);
-    % Eq. 45
-    % Adapt covariance matrix C
-    C = (1-c1-cmu) * C + c1 * (pc*pc' + (1-hsig) * cc*(2-cc) * C) + cmu * (B*D*arz(:,arindex(1:mu))) * diag(weights) * (B*D*arz(:,arindex(1:mu)))';
-    % Adapt step-size sigma
-    sigma = sigma * exp((cs/damps)*(norm(ps)/chiN - 1))
-    if max_sigma < sigma
-        max_sigma = sigma;
-    end
-    if min_sigma > sigma
-        min_sigma = sigma;
-    end
-    % Eq. 44
-    % Update B and D from C
-    if counteval - eigeneval > lambda/(c1+cmu)/N/10 % to achieve O(Nˆ2)
-        eigeneval = counteval;
-        C=triu(C)+triu(C,1)'; % enforce symmetry
-        [B,D] = eig(C);
-        % eigen decomposition, B==normalized eigenvectors
-        D = diag(sqrt(diag(D))); % D contains standard deviations now
-    end
-    % Break, if fitness is good enough
-    % if arfitness(1) >= stopfitness
-    %     break;
-    % end
-    % Escape flat fitness, or better terminate?
-    if arfitness(1) == arfitness(ceil(0.7*lambda))
-        sigma = sigma * exp(0.2+cs/damps);
-        disp('warning: flat fitness, consider reformulating the objective');
-    end
-    disp([num2str(counteval) ': ' num2str(arfitness(1))]);
-    bestFit(iGen) = arfitness(1);
-    medianFit(iGen) = median(arfitness);
-    medianFit(iGen)
-    counteval
-    iGen = iGen + 1;
-end % while, end generation loop
 
-disp([num2str(counteval) ': ' num2str(arfitness(1))]);
-xmin = arx(:, arindex(1));
+p = 5;      % number of generation at which freq_success_mut is update
 
-max_sigma
-min_sigma
-% sorted_children(1)
-% append to output
+
+for iGen=1:maxGen
+%     iGen
+    
+    % Mutation
+    mutated_children = cma_es_mutation(meanInd, sigma, lambda,C);
+    fitness_mutated_children = mse(mutated_children, nacafoil);
+    
+    [fitness_sorted, indices] = sort(fitness_mutated_children);
+    sorted_children = mutated_children(indices(1:mu),:);
+
+    ind_minus_mean = sorted_children - meanInd;
+    for i = 1:mu
+        C_mu = C_mu + weights(i) * (ind_minus_mean(i,:)' * ind_minus_mean(i,:));
+    end
+    C_mu = C_mu/mu;
+    rank_mu_update = c_mu * (1/(mueff^2)) * C_mu;
+    
+    new_mean = weights*sorted_children;
+    p_C = (1 - c_C)*p_C +  sqrt(c_C * (2 - c_C) * mueff) * (new_mean - meanInd)/sigma;
+    
+    rank_1_update = c_1 * (p_C' * p_C);
+
+    if mod(iGen, p) == 0
+        p_sigma = (1-c_sigma)*p_sigma + (sqrt(c_sigma*(2-c_sigma)*mueff)) * (new_mean - meanInd)/sigma * sqrtm(C);
+        sigma = sigma * exp(c_sigma * (norm(p_sigma)/norm(randn(32,1)) - 1));
+    end
+    
+    C = (1 - c_1 - c_mu) * C + rank_1_update + rank_mu_update;
+    
+    
+    meanInd = new_mean;
+    meanInd(meanInd>0.5) = 0.5; 
+    meanInd(meanInd<-0.5) = -0.5; 
+    
+    
+    bestFit(iGen) = min(fitness_sorted);
+    medianFit(iGen) = median(fitness_sorted);
+    
+    disp([iGen min(fitness_sorted) sigma])
+    
+%     plotFoil(nacafoil, meanInd);
+end
 output.bestFit = bestFit;
 output.medianFit = medianFit;
-output.elite = arx(:,1)';
+output.elite = meanInd;
     
 end
