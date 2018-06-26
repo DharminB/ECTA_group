61cebf742098bfbfee343171d48220aa7eca7d92
Dharmin <dharmingeo@gmail.com>
2018-06-26 21:58:42 +0200

    edited NSGA2 and NSGA

--- assignment_4/code/NSGA2.m
+++ assignment_4/code/NSGA2.m
@@ -1,9 +1,10 @@
 function output = NSGA2(pop, popSize)
     R = pop;
     N = popSize;
+    r_size = size(R, 1);
     fitness = calculateFitness(R);
-%     front = domination_sort(fitness);
-    front = naive_domination_sort(fitness);
+    front = domination_sort(fitness);
+%     front = naive_domination_sort(fitness)
     i = 1;
     new_pop = [];
     b = [];
@@ -16,8 +17,12 @@ function output = NSGA2(pop, popSize)
     end
     distance = crowding_distance(fitness(front == i));
     [sorted_distances, indices] = sort(distance);
-    a = vertcat(a, fitness(indices(1:N-size(new_pop)),:));
-    new_pop = vertcat(new_pop, R(indices(1:N-size(new_pop)),:));
+    indices = flipud(indices);
+    somearray = 1:r_size;
+    actual_indices = somearray(front==i);
+    indices = actual_indices(indices);
+    a = vertcat(a, fitness(indices(1:N-size(new_pop, 1)),:));
+    new_pop = vertcat(new_pop, R(indices(1:N-size(new_pop, 1)),:));
     b = vertcat(b, front(front == i));
     b = b(1:popSize);
     output.new_pop = new_pop;
--- assignment_4/code/naive_domination_sort.m
+++ assignment_4/code/naive_domination_sort.m
@@ -1,18 +1,18 @@
 function front = naive_domination_sort(fitness)
-    i = 1
-    popSize = size(fitness,1)
-    front = zeros(popSize,1)
-    fitness_back_up = fitness
+    i = 1;
+    popSize = size(fitness,1);
+    front = zeros(popSize,1);
+    fitness_back_up = fitness;
     while sum(front == 0) > 0
         for j = 1:popSize
             % To prevent already assigned fronts being over written
             if front(j) == 0
-                front(j) = i
+                front(j) = i;
                 for k = 1:popSize
                     if j ~= k && front(k) == 0 || front(k) == i
 %                         if (fitness(k,1) > fitness(j,1) && fitness(k,2) >= fitness(j,2)) || (fitness(k,1) >= fitness(j,1) && fitness(k,2) > fitness(j,2))
                           if domination(fitness(k,:),fitness(j,:))
-                            front(j) = 0
+                            front(j) = 0;
                             break
                         end
                     end
@@ -24,7 +24,7 @@ function front = naive_domination_sort(fitness)
 %         array. So we keep this constant and just add an if condition to
 %         prevent overwritting of already assigned fronts.
 %         fitness = fitness(front == 0)
-        i = i + 1
+        i = i + 1;
 %         front
     end
 %     front
