81f95317490b9736cbce2b3f7dda270a5075951d
Dharmin <dharmingeo@gmail.com>
2018-07-10 21:53:00 +0200

    faster and plots for RNN

--- assignment_5/code/ActivateRNNet.m
+++ assignment_5/code/ActivateRNNet.m
@@ -1,34 +1,9 @@
-function output = ActivateRNNet(nodeAct, WeightVector, nF, nH)
+function output = ActivateRNNet(nodeAct, wMat, nF, nH)
 nInputs = 1; 
 nFeatures = nF;
 nHidden = nH; 
 nNode = nFeatures+nHidden;
 
-% inputVector = [scaledState(1) scaledState(3) scaledState(5)];
-
-wMat = zeros(nNode);
-
-count = 1;
-for i=1:nFeatures
-    for j=nFeatures+1:nFeatures+nHidden
-        wMat(i,j) = WeightVector(1,count);
-        count = count + 1;
-    end
-end
-for i=nFeatures+1:nFeatures+nHidden
-    for j=nFeatures+1:nFeatures+nHidden
-        if i ~= j
-            wMat(i,j) = WeightVector(1,count);
-            count = count + 1;
-        end
-    end
-end
-
-% nodeAct = zeros(nInputs,nNode);
-% nodeAct(1:nFeatures) = inputVector;
-% for i = 1:nNode*nNode
-%     nodeAct = tanh(nodeAct*wMat);
-% end
 nodeAct = tanh(nodeAct*wMat);
 output.action = nodeAct(nNode);
 output.nodeAct = nodeAct;
